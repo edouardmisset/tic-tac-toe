bfe5bdc6268c26ebbd17a1a6b4f752c63c6c6a94
Edouard Misset <edouardmisset@gmail.com>
2021-11-16 15:18:38 +0100

    Added PWA

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -4,14 +4,14 @@ import { Component, Input } from '@angular/core';
   selector: 'app-square',
   template: `
     <button nbButton *ngIf="!value">{{ value }}</button>
-    <button nbButton hero status="success" *ngIf="value === 'X'">
+    <button nbButton hero status="success" *ngIf="value === 'O'">
       {{ value }}
     </button>
-    <button nbButton hero status="info" *ngIf="value === 'O'">
+    <button nbButton hero status="info" *ngIf="value === 'X'">
       {{ value }}
     </button>
   `,
-  styles: ['button {width:100%; height: 100%;}'],
+  styles: ['button {width:100%; height: 100%; font-size: 5em !important}'],
 })
 export class SquareComponent {
   @Input() value: 'X' | 'O';
